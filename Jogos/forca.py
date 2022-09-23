18f411c7b2fcc1b6b5bf55b502c865203d94d831
Evison <evison.pacheco21@gmail.com>
2022-09-23 14:25:50 -0300

    colocando os espaços da letra dentro de uma lista

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -4,19 +4,23 @@ def jogar():
     print("******************************")
 
     palavra_secreta = "maconha"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_", "_"]
 
     acertou = False
     enforcou = False
 
+    print(letras_acertadas)
+
     while(not acertou and not enforcou):
         chute = input("Escolha uma letra: ").lower().strip()
 
-        index = 1
+        index = 0
         for letra in palavra_secreta:
             if(chute == letra):
-                print(f"Encontrei a letra {letra.upper()} na posição {index}")
+                letras_acertadas[index] = letra
             index = index + 1
 
+        print(letras_acertadas)
 
 if(__name__ == "__main__"):
     jogar()
